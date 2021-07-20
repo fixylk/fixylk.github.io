7442a2beca125001752df92aa4aa249f1905337d
shyaman <shayaman321@gmail.com>
2021-07-20 10:04:17 +0530

    remove form submit js part

--- js/validation.js
+++ js/validation.js
@@ -1,15 +1,15 @@
-$(document).ready(function() {
-    $("#send_message").click(function(a) {
-		
+$(document).ready(function () {
+    $("#send_message").click(function (a) {
+
         a.preventDefault();
         var b = !1,
             c = $("#name").val(),
             d = $("#email").val(),
             e = $("#phone").val(),
             f = $("#message").val();
-        if ($("#name,#email,#phone,#message").click(function() {
-                $(this).removeClass("error_input")
-            }), 0 == c.length) {
+        if ($("#name,#email,#phone,#message").click(function () {
+            $(this).removeClass("error_input")
+        }), 0 == c.length) {
             var b = !0;
             $("#name").addClass("error_input")
         } else $("#name").removeClass("error_input");
@@ -25,11 +25,11 @@ $(document).ready(function() {
             var b = !0;
             $("#message").addClass("error_input")
         } else $("#message").removeClass("error_input");
-        0 == b && ($("#send_message").attr({
-            disabled: "true",
-            value: "Sending..."
-        }), $.post("email.php", $("#contact_form").serialize(), function(a) {
-            "sent" == a ? ($("#submit").remove(), $("#mail_success").fadeIn(500)) : ($("#mail_fail").fadeIn(500), $("#send_message").removeAttr("disabled").attr("value", "Send The Message"))
-        }))
+        // 0 == b && ($("#send_message").attr({
+        //     disabled: "true",
+        //     value: "Sending..."
+        // }), $.post("email.php", $("#contact_form").serialize(), function(a) {
+        //     "sent" == a ? ($("#submit").remove(), $("#mail_success").fadeIn(500)) : ($("#mail_fail").fadeIn(500), $("#send_message").removeAttr("disabled").attr("value", "Send The Message"))
+        // }))
     })
 });
